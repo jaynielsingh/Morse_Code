a6c3de902d98fa2b4e4382e448129e77fa2225dc
Shawn <jaynielsingh@yahoo.com>
2022-03-18 14:24:29 -0700

    update var

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ALPHABET_TO_MC = {
+ALPHABET = {
     "a": ".- ", "b": "-... ", "c": "-.-. ", "d": "-.. ", "e": ". ", "f": "..-. ", "g": "--. ", "h": ".... ", "i": ".. ",
     "j": ".--- ", "k": "-.- ", "l": ".-.. ", "m": "-- ", "n": "-. ", "o": "--- ", "p": ".--. ", "q": "--.- ",
     "r": ".-. ",
@@ -13,7 +13,7 @@ def encrypt(message):
     """Returns the morse code version of the character"""
     morse_code = ""
     for char in message:
-        char_code = ALPHABET_TO_MC[char]
+        char_code = ALPHABET[char]
         morse_code += char_code
     print(morse_code)
 
